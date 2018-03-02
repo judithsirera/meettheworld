65d555a04fb4ab91a808b068ad4304ac13cc7de4
jsirera <judsirera@gmail.com>
2018-03-01 22:46:04 -0500

    autentication fix bug

--- js/instagramManager.js
+++ js/instagramManager.js
@@ -1,12 +1,12 @@
 var instagramManager = {
-  _TOKEN: 'instatravel_token',
-  _USERNAME: 'instatravel_username',
+  _TOKEN: "instatravel_token",
+  _USERNAME: "instatravel_username",
 
-  username: '',
-  token: '',
+  username: "",
+  token: "",
 
 
-  client_id: '37ef44c599b5494480e90749c720eb7f',
+  client_id: "37ef44c599b5494480e90749c720eb7f",
   redirect_uri: 'https://judsirera.github.io/instatravel/login/',
   requestApi: 'https://api.instagram.com/v1/users/self/?access_token=',
   type: "GET",
@@ -19,8 +19,8 @@ var instagramManager = {
       if (xhr.readyState == 4) {
         // JSON.parse does not evaluate the attacker's scripts.
         var resp = JSON.parse(xhr.responseText);
-        this.username = resp.data.username;
-        localStorage.setItem(this._USERNAME, this.username);
+        instagramManager.username = resp.data.username;
+        localStorage.setItem(this._USERNAME, instagramManager.username);
 
         firebaseManager.getData();
       }
